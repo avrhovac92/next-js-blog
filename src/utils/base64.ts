1287f7120c7c0c7faa1acabef8c30dcce6f76f8b
Aleksandar <aleksandar.vrhovac@slotmill.com>
2022-07-09 10:14:02 +0200

    Test commit

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,7 +1,7 @@
 export function encodeBase64(data: string) {
   return Buffer.from(data, "utf-8").toString("base64");
 }
-
+//
 export function decodeBase64(data: string) {
   return Buffer.from(data, "base64").toString("utf-8");
 }
